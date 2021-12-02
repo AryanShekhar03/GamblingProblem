f145498d65faf746e094b4b24a51e99ace6a949a
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2021-12-02 18:53:02 +0530

    stake of 00 everyday and bet  everygame

---
+++ Gambling Problem/Gambling.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gambling_Problem
+{
+    class Gambling
+    {
+         int stake = 100;
+         int bet = 1;
+    }
+}
---
+++ Gambling Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gambling_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
